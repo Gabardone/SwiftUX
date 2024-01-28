ddab00232235013267d25282fa3c2015f20f2bbc
Óscar Morales Vivó <oscarmv@mac.com>
2024-01-28 13:58:13 -0800

    Added VisionOS support, updated to Swift 5.9 toolchain.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,6 +11,7 @@ let package = Package(
         .macOS(.v11),
         .macCatalyst(.v14),
         .tvOS(.v14),
+        .visionOS(.v1),
         .watchOS(.v7)
     ],
     products: [
